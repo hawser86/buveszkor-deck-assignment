64f1bc8e63b56b1e68fce73597075d10d2e0eaa3
hawser <hawser86@gmail.com>
2019-07-21 21:56:18 +0200

    fill missing values with default value when loading csv

--- src/load-votes.js
+++ src/load-votes.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
 const parse = require('csv-parse/lib/sync');
+const { defaultTo } = require('lodash');
 
 module.exports = filePath => {
   const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
-  const parsedVotes = parse(fileContent, { columns: true });
+  const parsedVotes = parse(fileContent, { columns: true, cast: true, relax_column_count: true });
 
+  const defaultValue = 5;
   return parsedVotes.map(vote => ({
     name: vote['Hogy hívnak?'],
-    KS: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: pikk király'], 10),
-    QS: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: pikk dáma'], 10),
-    JS: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: pikk jumbó'], 10),
-    KH: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: kőr király'], 10),
-    QH: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: kőr dáma'], 10),
-    JH: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: kőr jumbó'], 10),
-    KC: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: treff király'], 10),
-    QC: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: treff dáma'], 10),
-    JC: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: treff jumbó'], 10),
-    KD: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: káró király'], 10),
-    QD: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: káró dáma'], 10),
-    JD: parseInt(vote['Mennyire szeretnél rákerülni a következő lapra: káró jumbó'], 10)
+    KS: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: pikk király'], defaultValue),
+    QS: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: pikk dáma'], defaultValue),
+    JS: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: pikk jumbó'], defaultValue),
+    KH: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: kőr király'], defaultValue),
+    QH: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: kőr dáma'], defaultValue),
+    JH: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: kőr jumbó'], defaultValue),
+    KC: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: treff király'], defaultValue),
+    QC: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: treff dáma'], defaultValue),
+    JC: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: treff jumbó'], defaultValue),
+    KD: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: káró király'], defaultValue),
+    QD: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: káró dáma'], defaultValue),
+    JD: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: káró jumbó'], defaultValue)
   }));
 };
--- src/load-votes.spec.js
+++ src/load-votes.spec.js
@@ -4,25 +4,48 @@ const loadVotes = require('./load-votes');
 jest.mock('fs');
 
 describe('loadVotes', function () {
-  beforeEach(() => {
-    const testFileContent =
-      `"Időbélyeg","Hogy hívnak?","Mennyire szeretnél rákerülni a következő lapra: pikk király","Mennyire szeretnél rákerülni a következő lapra: pikk dáma","Mennyire szeretnél rákerülni a következő lapra: pikk jumbó","Mennyire szeretnél rákerülni a következő lapra: kőr király","Mennyire szeretnél rákerülni a következő lapra: kőr dáma","Mennyire szeretnél rákerülni a következő lapra: kőr jumbó","Mennyire szeretnél rákerülni a következő lapra: treff király","Mennyire szeretnél rákerülni a következő lapra: treff dáma","Mennyire szeretnél rákerülni a következő lapra: treff jumbó","Mennyire szeretnél rákerülni a következő lapra: káró király","Mennyire szeretnél rákerülni a következő lapra: káró dáma","Mennyire szeretnél rákerülni a következő lapra: káró jumbó"
-      "2019/07/16 7:06:19 du. EET","Bűvész Béla","10","0","6","8","0","4","10","0","6","8","0","4"
-      "2019/07/16 7:11:57 du. EET","Varázsló Vili","8","1","2","10","1","7","7","0","2","5","0","3"`
-        .split('\n').map(l => l.trim()).join('\n');
-    fs.readFileSync.mockReturnValue(testFileContent);
+  describe('when all columns are filled', function () {
+    beforeEach(() => {
+      const testFileContent =
+        `"Időbélyeg","Hogy hívnak?","Mennyire szeretnél rákerülni a következő lapra: pikk király","Mennyire szeretnél rákerülni a következő lapra: pikk dáma","Mennyire szeretnél rákerülni a következő lapra: pikk jumbó","Mennyire szeretnél rákerülni a következő lapra: kőr király","Mennyire szeretnél rákerülni a következő lapra: kőr dáma","Mennyire szeretnél rákerülni a következő lapra: kőr jumbó","Mennyire szeretnél rákerülni a következő lapra: treff király","Mennyire szeretnél rákerülni a következő lapra: treff dáma","Mennyire szeretnél rákerülni a következő lapra: treff jumbó","Mennyire szeretnél rákerülni a következő lapra: káró király","Mennyire szeretnél rákerülni a következő lapra: káró dáma","Mennyire szeretnél rákerülni a következő lapra: káró jumbó"
+        "2019/07/16 7:06:19 du. EET","Bűvész Béla","10","0","6","8","0","4","10","0","6","8","0","4"
+        "2019/07/16 7:11:57 du. EET","Varázsló Vili","8","1","2","10","1","7","7","0","2","5","0","3"`
+          .split('\n').map(l => l.trim()).join('\n');
+      fs.readFileSync.mockReturnValue(testFileContent);
+    });
+
+    it('should load file from given path', function () {
+      loadVotes('path/to/file');
+      expect(fs.readFileSync).toHaveBeenCalledWith('path/to/file', { encoding: 'utf-8' });
+    });
+
+    it('should return parsed votes', function () {
+      const votes = loadVotes('');
+      expect(votes).toEqual([
+        { name: 'Bűvész Béla', KS: 10, QS: 0, JS: 6, KH: 8, QH: 0, JH: 4, KC: 10, QC: 0, JC: 6, KD: 8, QD: 0, JD: 4 },
+        { name: 'Varázsló Vili', KS: 8, QS: 1, JS: 2, KH: 10, QH: 1, JH: 7, KC: 7, QC: 0, JC: 2, KD: 5, QD: 0, JD: 3 }
+      ]);
+    });
   });
 
-  it('should load file from given path', function () {
-    loadVotes('path/to/file');
-    expect(fs.readFileSync).toHaveBeenCalledWith('path/to/file', { encoding: 'utf-8' });
-  });
+  describe('when some columns are not filled', function () {
+    beforeEach(() => {
+      const testFileContent =
+        `"Időbélyeg","Hogy hívnak?","Mennyire szeretnél rákerülni a következő lapra: pikk király","Mennyire szeretnél rákerülni a következő lapra: pikk dáma","Mennyire szeretnél rákerülni a következő lapra: pikk jumbó","Mennyire szeretnél rákerülni a következő lapra: kőr király","Mennyire szeretnél rákerülni a következő lapra: kőr dáma","Mennyire szeretnél rákerülni a következő lapra: kőr jumbó","Mennyire szeretnél rákerülni a következő lapra: treff király","Mennyire szeretnél rákerülni a következő lapra: treff dáma","Mennyire szeretnél rákerülni a következő lapra: treff jumbó","Mennyire szeretnél rákerülni a következő lapra: káró király","Mennyire szeretnél rákerülni a következő lapra: káró dáma","Mennyire szeretnél rákerülni a következő lapra: káró jumbó"
+        "2019/07/16 7:06:19 du. EET","Bűvész Béla","10","0","6","8","0","4","10","0","6","8","0","4"
+        "2019/07/16 7:11:57 du. EET","Mentalista Márk","8","1","2","10","1","7","7","0","2"
+        "2019/07/16 7:11:57 du. EET","Varázsló Vili"`
+          .split('\n').map(l => l.trim()).join('\n');
+      fs.readFileSync.mockReturnValue(testFileContent);
+    });
 
-  it('should return parsed votes', function () {
-    const votes = loadVotes('');
-    expect(votes).toEqual([
-      { name: 'Bűvész Béla', KS: 10, QS: 0, JS: 6, KH: 8, QH: 0, JH: 4, KC: 10, QC: 0, JC: 6, KD: 8, QD: 0, JD: 4 },
-      { name: 'Varázsló Vili', KS: 8, QS: 1, JS: 2, KH: 10, QH: 1, JH: 7, KC: 7, QC: 0, JC: 2, KD: 5, QD: 0, JD: 3 }
-    ]);
+    it('should fill missing values with 5', function () {
+      const votes = loadVotes('');
+      expect(votes).toEqual([
+        { name: 'Bűvész Béla', KS: 10, QS: 0, JS: 6, KH: 8, QH: 0, JH: 4, KC: 10, QC: 0, JC: 6, KD: 8, QD: 0, JD: 4 },
+        { name: 'Mentalista Márk', KS: 8, QS: 1, JS: 2, KH: 10, QH: 1, JH: 7, KC: 7, QC: 0, JC: 2, KD: 5, QD: 5, JD: 5 },
+        { name: 'Varázsló Vili', KS: 5, QS: 5, JS: 5, KH: 5, QH: 5, JH: 5, KC: 5, QC: 5, JC: 5, KD: 5, QD: 5, JD: 5 }
+      ]);
+    });
   });
 });
