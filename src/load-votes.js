d4292f544322bc7c19107c044d51e65e2f79248d
hawser <hawser86@gmail.com>
2019-07-27 17:09:02 +0200

    do not load all the columns if they are not present in the header

--- src/load-votes.js
+++ src/load-votes.js
@@ -1,25 +1,40 @@
 const fs = require('fs');
 const parse = require('csv-parse/lib/sync');
-const { defaultTo } = require('lodash');
+const { defaultTo, without, zipObject } = require('lodash');
+
+const DEFAULT_VALUE = 5;
 
 module.exports = filePath => {
   const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
   const parsedVotes = parse(fileContent, { columns: true, cast: true, relax_column_count: true });
+  const columns = parseColumns(fileContent);
+  const keys = columns.map(questionToKey);
+  return parsedVotes
+    .map(vote => {
+      const values = columns.map(column => defaultTo(vote[column], DEFAULT_VALUE));
+      return zipObject(keys, values);
+    });
+};
 
-  const defaultValue = 5;
-  return parsedVotes.map(vote => ({
-    name: vote['Hogy hívnak?'],
-    KS: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: pikk király'], defaultValue),
-    QS: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: pikk dáma'], defaultValue),
-    JS: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: pikk jumbó'], defaultValue),
-    KH: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: kőr király'], defaultValue),
-    QH: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: kőr dáma'], defaultValue),
-    JH: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: kőr jumbó'], defaultValue),
-    KC: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: treff király'], defaultValue),
-    QC: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: treff dáma'], defaultValue),
-    JC: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: treff jumbó'], defaultValue),
-    KD: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: káró király'], defaultValue),
-    QD: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: káró dáma'], defaultValue),
-    JD: defaultTo(vote['Mennyire szeretnél rákerülni a következő lapra: káró jumbó'], defaultValue)
-  }));
+const parseColumns = fileContent => {
+  const [parsedColumns] = parse(fileContent, { columns: false, cast: true, from_line: 1, to_line: 1 });
+  return without(parsedColumns, 'Időbélyeg');
+};
+
+const questionToKey = question => {
+  switch (question) {
+    case 'Hogy hívnak?': return 'name';
+    case 'Mennyire szeretnél rákerülni a következő lapra: pikk király': return 'KS';
+    case 'Mennyire szeretnél rákerülni a következő lapra: pikk dáma': return 'QS';
+    case 'Mennyire szeretnél rákerülni a következő lapra: pikk jumbó': return 'JS';
+    case 'Mennyire szeretnél rákerülni a következő lapra: kőr király': return 'KH';
+    case 'Mennyire szeretnél rákerülni a következő lapra: kőr dáma': return 'QH';
+    case 'Mennyire szeretnél rákerülni a következő lapra: kőr jumbó': return 'JH';
+    case 'Mennyire szeretnél rákerülni a következő lapra: treff király': return 'KC';
+    case 'Mennyire szeretnél rákerülni a következő lapra: treff dáma': return 'QC';
+    case 'Mennyire szeretnél rákerülni a következő lapra: treff jumbó': return 'JC';
+    case 'Mennyire szeretnél rákerülni a következő lapra: káró király': return 'KD';
+    case 'Mennyire szeretnél rákerülni a következő lapra: káró dáma': return 'QD';
+    case 'Mennyire szeretnél rákerülni a következő lapra: káró jumbó': return 'JD';
+  }
 };
--- src/load-votes.spec.js
+++ src/load-votes.spec.js
@@ -31,9 +31,9 @@ describe('loadVotes', function () {
   describe('when some columns are not filled', function () {
     beforeEach(() => {
       const testFileContent =
-        `"Időbélyeg","Hogy hívnak?","Mennyire szeretnél rákerülni a következő lapra: pikk király","Mennyire szeretnél rákerülni a következő lapra: pikk dáma","Mennyire szeretnél rákerülni a következő lapra: pikk jumbó","Mennyire szeretnél rákerülni a következő lapra: kőr király","Mennyire szeretnél rákerülni a következő lapra: kőr dáma","Mennyire szeretnél rákerülni a következő lapra: kőr jumbó","Mennyire szeretnél rákerülni a következő lapra: treff király","Mennyire szeretnél rákerülni a következő lapra: treff dáma","Mennyire szeretnél rákerülni a következő lapra: treff jumbó","Mennyire szeretnél rákerülni a következő lapra: káró király","Mennyire szeretnél rákerülni a következő lapra: káró dáma","Mennyire szeretnél rákerülni a következő lapra: káró jumbó"
-        "2019/07/16 7:06:19 du. EET","Bűvész Béla","10","0","6","8","0","4","10","0","6","8","0","4"
-        "2019/07/16 7:11:57 du. EET","Mentalista Márk","8","1","2","10","1","7","7","0","2"
+        `"Időbélyeg","Hogy hívnak?","Mennyire szeretnél rákerülni a következő lapra: pikk király","Mennyire szeretnél rákerülni a következő lapra: pikk dáma","Mennyire szeretnél rákerülni a következő lapra: pikk jumbó"
+        "2019/07/16 7:06:19 du. EET","Bűvész Béla","10","0","6"
+        "2019/07/16 7:11:57 du. EET","Mentalista Márk","8","1"
         "2019/07/16 7:11:57 du. EET","Varázsló Vili"`
           .split('\n').map(l => l.trim()).join('\n');
       fs.readFileSync.mockReturnValue(testFileContent);
@@ -42,9 +42,9 @@ describe('loadVotes', function () {
     it('should fill missing values with 5', function () {
       const votes = loadVotes('');
       expect(votes).toEqual([
-        { name: 'Bűvész Béla', KS: 10, QS: 0, JS: 6, KH: 8, QH: 0, JH: 4, KC: 10, QC: 0, JC: 6, KD: 8, QD: 0, JD: 4 },
-        { name: 'Mentalista Márk', KS: 8, QS: 1, JS: 2, KH: 10, QH: 1, JH: 7, KC: 7, QC: 0, JC: 2, KD: 5, QD: 5, JD: 5 },
-        { name: 'Varázsló Vili', KS: 5, QS: 5, JS: 5, KH: 5, QH: 5, JH: 5, KC: 5, QC: 5, JC: 5, KD: 5, QD: 5, JD: 5 }
+        { name: 'Bűvész Béla', KS: 10, QS: 0, JS: 6},
+        { name: 'Mentalista Márk', KS: 8, QS: 1, JS: 5},
+        { name: 'Varázsló Vili', KS: 5, QS: 5, JS: 5 }
       ]);
     });
   });
